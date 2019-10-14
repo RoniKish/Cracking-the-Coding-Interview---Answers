74f7a85c87cfa20500006a60eae31d628f5a517c
RoniKish <R_k@gmail.com>
2019-10-14 22:23:06 +0300

    Back to solving in Java this time

---
+++ Chapter_4/binary_tree_layout.java
@@ -0,0 +1,75 @@
+// Implementing a simple binary tree
+package com.company;
+
+public class binary_tree_layout {
+
+    Node rootNode;
+
+    public binary_tree_layout() {
+        this.rootNode = null;
+    }
+
+    public void add(int value) {
+        this.rootNode = addRecursive(this.rootNode, value);
+    }
+
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+        if (value <= current.value)
+            current.leftChild = addRecursive(current.leftChild, value);
+        else
+            current.rightChild = addRecursive(current.rightChild, value);
+
+        return current;
+    }
+
+    public void inOrderTraversal(Node currentNode) {
+        if (currentNode != null) {
+            inOrderTraversal(currentNode.leftChild);
+            System.out.println(currentNode.value);
+            inOrderTraversal(currentNode.rightChild);
+        }
+    }
+
+    public void preOrderTraversal(Node currentNode) {
+        if (currentNode != null) {
+            System.out.println(currentNode.value);
+            inOrderTraversal(currentNode.leftChild);
+            inOrderTraversal(currentNode.rightChild);
+        }
+    }
+
+    public void postOrderTraversal(Node currentNode) {
+        if (currentNode != null) {
+            inOrderTraversal(currentNode.leftChild);
+            inOrderTraversal(currentNode.rightChild);
+            System.out.println(currentNode.value);
+        }
+    }
+
+    public static void main(String[] args) {
+        binary_tree_layout binaryTree = new binary_tree_layout();
+        binaryTree.add(5);
+        binaryTree.add(1);
+        binaryTree.add(10);
+        binaryTree.inOrderTraversal(binaryTree.rootNode);
+        System.out.println();
+        binaryTree.preOrderTraversal(binaryTree.rootNode);
+        System.out.println();
+        binaryTree.postOrderTraversal(binaryTree.rootNode);
+    }
+}
+
+class Node {
+    int value;
+    Node leftChild;
+    Node rightChild;
+
+    public Node(int value) {
+        this.value = value;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+}
